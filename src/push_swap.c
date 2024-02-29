845b651ce030748eab017353db20d6476367a02e
Tulece <antoine.porceddu@hotmail.ca>
2024-02-29 12:52:48 +0100

    coucou antoine

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: anporced <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:50:12 by anporced          #+#    #+#             */
-/*   Updated: 2024/02/29 12:13:26 by anporced         ###   ########.fr       */
+/*   Updated: 2024/02/29 12:50:34 by anporced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,5 @@ int	main(int ac, char **av)
 	if (is_sorted(&ab))
 		return (0);
 	algorithm(&ab);
-	while (!is_sorted(&ab))
-		ra(&ab);
 	return (0);
 }
