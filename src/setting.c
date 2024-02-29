94531d846c7e72fa734fd5a31d6cee343f266d0d
Tulece <antoine.porceddu@hotmail.ca>
2024-02-29 15:53:13 +0100

    swap

--- src/setting.c
+++ src/setting.c
@@ -6,7 +6,7 @@
 /*   By: anporced <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:40:52 by anporced          #+#    #+#             */
-/*   Updated: 2024/02/28 18:17:18 by anporced         ###   ########.fr       */
+/*   Updated: 2024/02/29 15:52:58 by anporced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,24 @@ void	set_cost(t_ab *ab)
 			else
 				current->target->cost
 					= lst_size(ab->lst_a) - current->target->index;
+			if ((current->index <= lst_size(ab->lst_b) / 2
+					&& current->target->index <= lst_size(ab->lst_a) / 2)
+				|| (current->index > lst_size(ab->lst_b) / 2
+					&& current->target->index > lst_size(ab->lst_a) / 2))
+			{
+				if (current->cost <= current->target->cost)
+					current->cost = current->target->cost;
+			}
+			else
+				current->cost = current->cost + current->target->cost;
+		}
+		else
+		{
+			if (current->target->index > lst_size(ab->lst_a) / 2)
+				current->target->cost
+					= lst_size(ab->lst_a) - current->target->index;
+			else
+				current->target->cost = current->target->index;
 			if ((current->index <= lst_size(ab->lst_b) / 2
 					&& current->target->index <= lst_size(ab->lst_a) / 2)
 				|| (current->index > lst_size(ab->lst_b) / 2
