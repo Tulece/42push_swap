25f03d0087c2c8fc2a6020a66fc500f43b4f580f
Tulece <antoine.porceddu@hotmail.ca>
2024-02-29 17:08:49 +0100

    psswwpwc

--- src/setting.c
+++ src/setting.c
@@ -6,7 +6,7 @@
 /*   By: anporced <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:40:52 by anporced          #+#    #+#             */
-/*   Updated: 2024/02/29 15:52:58 by anporced         ###   ########.fr       */
+/*   Updated: 2024/02/29 17:07:25 by anporced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,22 @@ void	set_index(t_ab *ab)
 	}
 }
 
-void	set_cost(t_ab *ab)
+void	set_cost_a(t_ab *ab)
+{
+	t_lst	*current;
+
+	current = ab->lst_a;
+	while (current)
+	{
+		if (current->index <= lst_size(ab->lst_a) / 2)
+			current->cost = current->index;
+		else
+			current->cost = lst_size(ab->lst_a) - current->index;
+		current = current->next;
+	}
+}
+
+void	set_cost_b(t_ab *ab)
 {
 	t_lst	*current;
 
@@ -43,41 +58,61 @@ void	set_cost(t_ab *ab)
 	while (current)
 	{
 		if (current->index <= lst_size(ab->lst_b) / 2)
-		{
-			if (current->target->index <= lst_size(ab->lst_a) / 2)
-				current->target->cost = current->target->index;
-			else
-				current->target->cost
-					= lst_size(ab->lst_a) - current->target->index;
-			if ((current->index <= lst_size(ab->lst_b) / 2
-					&& current->target->index <= lst_size(ab->lst_a) / 2)
-				|| (current->index > lst_size(ab->lst_b) / 2
-					&& current->target->index > lst_size(ab->lst_a) / 2))
-			{
-				if (current->cost <= current->target->cost)
-					current->cost = current->target->cost;
-			}
-			else
-				current->cost = current->cost + current->target->cost;
-		}
+			current->cost = current->index;
 		else
-		{
-			if (current->target->index > lst_size(ab->lst_a) / 2)
-				current->target->cost
-					= lst_size(ab->lst_a) - current->target->index;
-			else
-				current->target->cost = current->target->index;
-			if ((current->index <= lst_size(ab->lst_b) / 2
-					&& current->target->index <= lst_size(ab->lst_a) / 2)
-				|| (current->index > lst_size(ab->lst_b) / 2
-					&& current->target->index > lst_size(ab->lst_a) / 2))
-			{
-				if (current->cost <= current->target->cost)
-					current->cost = current->target->cost;
-			}
-			else
-				current->cost = current->cost + current->target->cost;
-		}
+			current->cost = lst_size(ab->lst_b) - current->index;
 		current = current->next;
 	}
 }
+
+void	set_cost(t_ab *ab)
+{
+	set_cost_b(ab);
+	set_cost_a(ab);
+}
+// void	set_cost(t_ab *ab)
+// {
+// 	t_lst	*current;
+
+// 	current = ab->lst_b;
+// 	while (current)
+// 	{
+// 		if (current->index <= lst_size(ab->lst_b) / 2)
+// 		{
+// 			if (current->target->index <= lst_size(ab->lst_a) / 2)
+// 				current->target->cost = current->target->index;
+// 			else
+// 				current->target->cost
+// 					= lst_size(ab->lst_a) - current->target->index;
+// 			if ((current->index <= lst_size(ab->lst_b) / 2
+// 					&& current->target->index <= lst_size(ab->lst_a) / 2)
+// 				|| (current->index > lst_size(ab->lst_b) / 2
+// 					&& current->target->index > lst_size(ab->lst_a) / 2))
+// 			{
+// 				if (current->cost <= current->target->cost)
+// 					current->cost = current->target->cost;
+// 			}
+// 			else
+// 				current->cost = current->cost + current->target->cost;
+// 		}
+// 		else
+// 		{
+// 			if (current->target->index > lst_size(ab->lst_a) / 2)
+// 				current->target->cost
+// 					= lst_size(ab->lst_a) - current->target->index;
+// 			else
+// 				current->target->cost = current->target->index;
+// 			if ((current->index <= lst_size(ab->lst_b) / 2
+// 					&& current->target->index <= lst_size(ab->lst_a) / 2)
+// 				|| (current->index > lst_size(ab->lst_b) / 2
+// 					&& current->target->index > lst_size(ab->lst_a) / 2))
+// 			{
+// 				if (current->cost <= current->target->cost)
+// 					current->cost = current->target->cost;
+// 			}
+// 			else
+// 				current->cost = current->cost + current->target->cost;
+// 		}
+// 		current = current->next;
+// 	}
+// }
